de109fa2c2363f27d0ca6a7167b0aa9edbb2f1a0
Yukusai <85849071+p-yukusai@users.noreply.github.com>
2024-05-10 14:20:21 -0400

    - Add serializer and deserializer for audio config struct

--- src/gui/AudioPlaybackWidget.cpp
+++ src/gui/AudioPlaybackWidget.cpp
@@ -3,5 +3,47 @@
 //
 
 #include "AudioPlaybackWidget.h"
+#include <QJsonObject>
+#include <QJsonDocument>
 
-namespace MusWidget {} // namespace MusWidget
+bool AudioPlaybackWidget::serialize(std::vector<audioConfig>* pConf, const QString& outPath) {
+    QJsonObject audioJson;
+    audioJson["Tracks"] = (int)pConf->size();
+    int x = 0;
+    for(const auto& audioConfig: *pConf){
+        QJsonObject track;
+        track["Name"] = audioConfig.audioName;
+        track["Path"] = audioConfig.audioPath.absolutePath();
+        track["Playback"] = audioConfig.playbackEnable;
+        track["StartFrame"] = audioConfig.startFrame;
+        track["EndFrame"] = audioConfig.endFrame;
+        track["Duration"] = audioConfig.durationInFrames;
+        audioJson[QString::number(x)] = track;
+        x++;
+    }
+    QFile file(outPath);
+    file.open(QIODevice::ReadWrite);
+    file.write(QJsonDocument(audioJson).toJson(QJsonDocument::Indented));
+    file.close();
+    return false;
+}
+bool AudioPlaybackWidget::deserialize(const QJsonObject& pConf) const {
+    if(pConf.isEmpty()) { return false; }
+    try {
+        auto deserialized = std::vector<audioConfig>();
+        for (int x = 0; x > pConf["Tracks"].toInt(); x++){
+            deserialized.emplace_back();
+            deserialized.at(x).audioName = pConf[QString::number(x)]["Name"].toString();
+            deserialized.at(x).audioPath = QFileInfo(pConf[QString::number(x)]["Path"].toString());
+            deserialized.at(x).playbackEnable = pConf[QString::number(x)]["Playback"].toBool();
+            deserialized.at(x).startFrame = pConf[QString::number(x)]["StartFrame"].toInt();
+            deserialized.at(x).endFrame = pConf[QString::number(x)]["EndFrame"].toInt();
+            deserialized.at(x).durationInFrames = pConf[QString::number(x)]["Duration"].toInt();
+        }
+        playbackConfig->clear();
+        *playbackConfig = deserialized;
+    }
+    // TODO: Fix this later
+    catch (...) { return false; }
+    return true;
+}
